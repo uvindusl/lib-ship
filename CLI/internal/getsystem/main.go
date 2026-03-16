601c34b618ddd34a5d7eae7dd131abb426399f09
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2026-03-16 12:19:57 +0530

    remove that description text

--- CLI/internal/getsystem/main.go
+++ CLI/internal/getsystem/main.go
@@ -3,15 +3,22 @@ package getsystem
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func Getsysteminfo() string {
 	cmd := exec.Command("lsb_release", "-d")
+
 	stdout, err := cmd.Output()
+	
+	systeminfo := string(stdout)
+	
+	systeminfo = strings.Trim(systeminfo, "Description:")
+	
 	if err != nil {
 		fmt.Println(err)
 		return ""
 	}
 
-	return string(stdout)
+	return string(systeminfo)
 }
